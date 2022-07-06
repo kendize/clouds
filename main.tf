44fffa90f4ac96f2fd70d866d80d87960ce26b1b
Yurii <46537233+kendize@users.noreply.github.com>
2022-07-06 19:01:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,51 @@ resource "aws_instance" "application" {
   	Name = "application"
   }
 }
+resource "aws_security_group" "security_demo" {
+   name = "WS SG"
+   description = "workid"
+
+  ingress {
+    from_port   = 8081
+    protocol    = "tcp"
+    to_port     = 8081
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port   = 3000
+    protocol    = "tcp"
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  
+    ingress {
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+    ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
