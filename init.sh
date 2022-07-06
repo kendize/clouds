c7daff0bf0c26218f74a9b323a18936235912456
Yurii <46537233+kendize@users.noreply.github.com>
2022-07-06 18:51:32 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt install -y docker.io
 sudo apt install -y docker-compose
 sudo git clone https://github.com/kendize/clouds && cd clouds
-sudo apt install aws-cli
+sudo apt-get install awscli
 sudo aws configure
 sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 065659108092.dkr.ecr.us-east-1.amazonaws.com
 
